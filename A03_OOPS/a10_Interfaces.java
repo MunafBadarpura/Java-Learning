bf1e0ab84a992a44cabeae046ca0b69364822461
MunafBadarpura <munafbadarpura11@gmail.com>
2024-08-20 08:06:42 +0530

    first commit

---
+++ A03_OOPS/a10_Interfaces.java
@@ -0,0 +1,45 @@
+interface chessPlayer{
+    void moves();
+}
+
+class Queen implements chessPlayer {
+    public void moves(){
+        System.out.println("up, down, left, right, diognal");
+    }
+}
+class Rook implements chessPlayer {
+    public void moves(){
+        System.out.println("up, down, left, right");
+    }
+}
+class King implements chessPlayer {
+    public void moves(){
+        System.out.println("up, down, left, right, diognal(by 1 step)");
+    }
+}
+
+// Multiple Inheretance 
+interface Animal{
+    void eat();
+}
+
+interface Human{
+    void breath();
+}
+
+class Dog implements Animal, Human{
+    public void eat(){
+        System.out.println("dog eats");
+    }
+    public void breath(){
+        System.out.println("dog breath");
+    }
+}
+
+
+public class a10_Interfaces {
+    public static void main(String[] args) {
+        Queen q = new Queen();
+        q.moves();
+    }
+}
---
+++ A03_OOPS/a11_Static_Keyword.java
@@ -0,0 +1,5 @@
+public class a11_Static_Keyword {
+    public static void main(String[] args) {
+        
+    }
+}
