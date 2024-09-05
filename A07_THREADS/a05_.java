7fc2a0883c762d968bd44f3ece10e6d3c0f08d3d
MunafBadarpura <munafbadarpura11@gmail.com>
2024-09-05 08:02:22 +0530

    date : 05-09-2024

--- A07_THREADS/a05_.java
+++ A07_THREADS/a05_.java
@@ -5,9 +5,19 @@ public class a05_ extends Thread{
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         a05_ a = new a05_();
         System.out.println(a.getState());
 
+        a.start();
+        System.out.println(a.getState());
+        System.out.println(Thread.currentThread().getState());
+        Thread.sleep(100);
+        System.out.println(Thread.currentThread().getState());
+
+        a.join();
+        System.out.println(a.getState());
+
+       
     }
 }
