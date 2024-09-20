49b5d885aa02d774c8df06700241e84ad4e65a4a
MunafBadarpura <munafbadarpura11@gmail.com>
2024-09-20 17:48:33 +0530

    date : 20-09-2024

--- Z-C/a04.c
+++ Z-C/a04.c
@@ -7,20 +7,41 @@
 
 #include<stdio.h>
 int main(){
-    // char str[6] = "fsdfs";
-
-    // printf("%c\n", 2[str]);
-
-    // puts(str);
-
 
     char str[10];
     puts("Enter String : ");
     scanf("%s", &str);
-    //scanf("%[^\n]s", str);
-    //gets(str);
 
-    printf("%s", str);
+    int num1 = 0;
+    int num2 = 0;
+    int check = 0;
+    char opr = '0';
+
+    int i = 0;
+    while(str[i] != '\n'){
+        if(str[i] >= '0' && str[i] <= '9'){
+            int val = str[i] - 48;
+            if(check == 0){
+                num1 =  num1 *10 + val;
+            }
+            if(check == 1){
+                printf("%d\n", val);
+                num2 =  num2 *10 + val;
+            }
+        }
+
+        else if(str[i] == '+' || str[i] == '-'){
+            check = 1;
+            opr = str[i];
+        }
+
+        i++;
+    }
+
+
+    printf("%d", num1);
+    printf(" %d", num2);
+    printf(" %c", opr);
 
 
 
